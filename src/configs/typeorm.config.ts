1c93d103971264032bca3e9ad83e7265490dba8b
Bhayron Klivilan <bhayronklivilan@gmail.com>
2022-03-09 19:02:07 -0300

    module users

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: 'localhost',
-  port: 5433,
+  port: 5432,
   username: 'bhayron',
   password: 'secretf5',
   database: 'nestjs',
