b50d39a92834a96d96bf60f64849a9a093f44b68
bhayron <bhayronklivilan@gmail.com>
2022-03-10 15:14:39 -0300

    Docker

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,10 +2,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'pgsql',
   port: 5432,
   username: 'bhayron',
-  password: 'secretf5',
+  password: 'secret',
   database: 'nestjs',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
